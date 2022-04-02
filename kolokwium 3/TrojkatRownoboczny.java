568e506500b4e41743238b13488075db8e7a22e9
s26234 <95536634+jachu11@users.noreply.github.com>
2022-04-02 13:20:45 +0200

    Create TrojkatRownoboczny.java

---
+++ kolokwium 3/TrojkatRownoboczny.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class TrojkatRownoboczny { double x; double y;
+
+    public TrojkatRownoboczny(double podstawa) {
+    }
+
+    public TrojkatRownoboczny(double podstawa, double podstawa1) {
+    }
+
+    public double wysokosc() {
+        return 0;
+    }
+}
