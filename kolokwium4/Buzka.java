ea027ca91675a47983301b2414fd3e0a5f861448
s26234 <95536634+jachu11@users.noreply.github.com>
2022-04-23 13:45:12 +0200

    Create Buzka.java

---
+++ kolokwium4/Buzka.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Buzka implements Rysowalny {
+    public Buzka(char c) {
+
+    }
+
+    public Buzka() {
+
+    }
+
+    public void draw() {
+    }
+
+    public boolean czySieUsmiecha() {
+        return false;
+    }
+
+    @Override
+    public void przestanSieUsmiechac() {
+
+    }
+
+}
