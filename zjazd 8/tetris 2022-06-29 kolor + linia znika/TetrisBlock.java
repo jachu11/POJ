49d11372ba90a8c514a5bcdd4f95c058a6e16090
s26234 <95536634+jachu11@users.noreply.github.com>
2022-06-29 23:17:51 +0200

    Create TetrisBlock.java

---
+++ zjazd 8/tetris 2022-06-29 kolor + linia znika/TetrisBlock.java
@@ -0,0 +1,117 @@
+package tetris;
+import java.awt.Color;  
+import java.util.Random;
+
+public class TetrisBlock 
+{
+    private int[][] shape;
+    private Color color;
+    private int x,y;
+    private int [][][] shapes;
+    private int currentRotation;
+    
+   private Color[] availableColors = {Color.green, Color.red, Color.blue,};
+
+        public TetrisBlock(int[][] shape, Color color)
+        {
+            this.shape = shape;
+            this.color = color;
+            
+            initShapes();
+        }
+    
+        
+    public void initShapes ()
+    {    
+        shapes = new int[4][][];
+        
+        for (int i = 0; i < 4; i++)
+        {
+            int r = shape[0].length;
+            int c = shape.length;
+            
+            shapes[i] = new int [r][c];
+            
+            for (int y = 0; y < r; y++ )
+            {
+                for (int x = 0; x < c; x++)
+                {
+                shapes[i][y][x] = shape [c - x - 1][y];
+                }
+            }    
+        shape = shapes[i];
+        }
+        
+    }
+    
+    public void spawn(int gridWidth)
+    {
+        Random r = new Random();
+        
+        currentRotation = r.nextInt (shapes.length);
+        shape = shapes [currentRotation];
+        
+     
+        y = 0 - getHeight();
+        x = r.nextInt(gridWidth - getWidth());
+        
+        color = availableColors [r.nextInt(availableColors.length )];
+        
+    }
+        
+    public int[][] getShape() {
+       return shape;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+    
+    public int getHeight()      {
+        return shape.length;    }
+    
+    public int getWidth()       {
+        return shape[0].length; }
+   
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public int moveDown() {
+        return y++; 
+    }
+    
+    public int moveLeft() {
+        return x--;
+    }
+    
+    public int moveRight() {
+        return x++;
+    }
+    
+    public void rotate()
+    {
+        currentRotation++;
+        if(currentRotation > 3) currentRotation = 0;
+        shape = shapes[currentRotation];
+                
+    }
+    
+    public int getBottomEdge() {
+        return y + getHeight();
+    }
+   
+    public int getLeftEdge() {
+        return x;
+    }
+     
+    public int getRightEdge() {
+        return x + getWidth();
+    }
+    
+    
+}
